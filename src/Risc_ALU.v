82ae6ff03e858a2ce7012b33b10a064ad963a55e
Jerome S <74451308+JeromeStephenson@users.noreply.github.com>
2026-03-12 20:57:05 +0530

    Create Risc_ALU.v

---
+++ src/Risc_ALU.v
@@ -0,0 +1,63 @@
+module ALU(A,B,Result,ALUControl,OverFlow,Carry,Zero,Negative);
+
+    input [31:0]A,B;
+    input [2:0]ALUControl;
+    output Carry,OverFlow,Zero,Negative;
+    output [31:0]Result;
+
+    wire Cout;
+    wire [31:0]Sum;
+
+    assign {Cout,Sum} = (ALUControl[0] == 1'b0) ? A + B :
+                                          (A + ((~B)+1)) ;
+    assign Result = (ALUControl == 3'b000) ? Sum :
+                    (ALUControl == 3'b001) ? Sum :
+                    (ALUControl == 3'b010) ? A & B :
+                    (ALUControl == 3'b011) ? A | B :
+                    (ALUControl == 3'b101) ? {{31{1'b0}},(Sum[31])} : {32{1'b0}};
+    
+    assign OverFlow = ((Sum[31] ^ A[31]) & 
+                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
+                      (~ALUControl[1]));
+    assign Carry = ((~ALUControl[1]) & Cout);
+    assign Zero = &(~Result);
+    assign Negative = Result[31];
+
+endmodule
+
+
+
+
+
+module ALU_Decoder(ALUOp,funct3,funct7,op,ALUControl);
+
+    input [1:0]ALUOp;
+    input [2:0]funct3;
+    input [6:0]funct7,op;
+    output [2:0]ALUControl;
+
+    // Method 1 
+    // assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
+    //                     (ALUOp == 2'b01) ? 3'b001 :
+    //                     (ALUOp == 2'b10) ? ((funct3 == 3'b000) ? ((({op[5],funct7[5]} == 2'b00) | ({op[5],funct7[5]} == 2'b01) | ({op[5],funct7[5]} == 2'b10)) ? 3'b000 : 3'b001) : 
+    //                                         (funct3 == 3'b010) ? 3'b101 : 
+    //                                         (funct3 == 3'b110) ? 3'b011 : 
+    //                                         (funct3 == 3'b111) ? 3'b010 : 3'b000) :
+    //                                        3'b000;
+
+    // Method 2
+    assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
+                        (ALUOp == 2'b01) ? 3'b001 :
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} == 2'b11)) ? 3'b001 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} != 2'b11)) ? 3'b000 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : 
+                                                                  3'b000 ;
+endmodule
+
+
+
+
+
+
